dde0bfa7fefb0c0b6aa15504351c6d9b78788a68
Miguel Ibarra <miguel@digitec.ws>
2018-09-18 22:13:00 -0700

    executing speedtest and saving in aws s3

---
+++ speedtest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import csv
+import decimal
+import subprocess
+
+
+response = subprocess.Popen('/home/pi/speedtest/bin/speedtest-cli --csv  --share', shell=True, stdout=subprocess.PIPE).stdout.read()
+
+reader = csv.reader([response], dialect='excel')
+
+for row in reader:
+        print '"{}","{}","{}","{}","{}","{}","{}","{}","{}","{}"'.format(row[0], row[1], row[2], row[3], row[4], row[5], decimal.Decimal(row[6])/1000000, decimal.Decimal(row[7])/1000000, row[8], row[9])
