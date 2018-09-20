8621af68ea18b0bd769cc23e4112e304f695032e
tecnom1k3 <miguel@digitec.ws>
2018-09-19 22:53:53 -0700

    Feature/python csv (#1)
    * improving python script

--- speedtest.py
+++ speedtest.py
@@ -3,11 +3,17 @@
 import csv
 import decimal
 import subprocess
+import StringIO
 
+def csv2string(data):
+    si = StringIO.StringIO()
+    cw = csv.writer(si)
+    cw.writerow(data)
+    return si.getvalue().strip('\r\n')
 
 response = subprocess.Popen('/home/pi/speedtest/bin/speedtest-cli --csv  --share', shell=True, stdout=subprocess.PIPE).stdout.read()
-
 reader = csv.reader([response], dialect='excel')
-
 for row in reader:
-        print '"{}","{}","{}","{}","{}","{}","{}","{}","{}","{}"'.format(row[0], row[1], row[2], row[3], row[4], row[5], decimal.Decimal(row[6])/1000000, decimal.Decimal(row[7])/1000000, row[8], row[9])
+	row[6] = decimal.Decimal(row[6])/1000000
+	row[7] = decimal.Decimal(row[7])/1000000
+	print csv2string(row)
