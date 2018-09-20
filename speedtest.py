bf5142f5db45a636e9209c72086fc37708e5e9ec
Miguel Ibarra <miguel@digitec.ws>
2018-09-20 03:55:02 +0000

    improving python script

--- speedtest.py
+++ speedtest.py
@@ -4,10 +4,7 @@ import csv
 import decimal
 import subprocess
 
-
-response = subprocess.Popen('/home/pi/speedtest/bin/speedtest-cli --csv  --share', shell=True, stdout=subprocess.PIPE).stdout.read()
-
-reader = csv.reader([response], dialect='excel')
-
-for row in reader:
-        print '"{}","{}","{}","{}","{}","{}","{}","{}","{}","{}"'.format(row[0], row[1], row[2], row[3], row[4], row[5], decimal.Decimal(row[6])/1000000, decimal.Decimal(row[7])/1000000, row[8], row[9])
+with subprocess.Popen('/home/pi/speedtest/bin/speedtest-cli --csv  --share', shell=True, stdout=subprocess.PIPE).stdout.read() as response:
+	reader = csv.reader([response], dialect='excel')
+	for row in reader:
+		print '"{}","{}","{}","{}","{}","{}","{}","{}","{}","{}"'.format(row[0], row[1], row[2], row[3], row[4], row[5], decimal.Decimal(row[6])/1000000, decimal.Decimal(row[7])/1000000, row[8], row[9])
