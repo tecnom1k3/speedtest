91896a43e1b7eeb4ae7cdbe76d44263c9ff8a2bb
Miguel Ibarra <miguel@digitec.ws>
2018-09-23 20:11:34 +0000

    adding .env

--- speedtest-rest.py
+++ speedtest-rest.py
@@ -5,6 +5,10 @@ import decimal
 import subprocess
 import StringIO
 import requests
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 def csv2string(data):
     si = StringIO.StringIO()
@@ -12,11 +16,13 @@ def csv2string(data):
     cw.writerow(data)
     return si.getvalue().strip('\r\n')
 
+endpoint = os.getenv("LOG_ENPOINT")
+
 response = subprocess.Popen('/home/pi/speedtest/bin/speedtest-cli --csv  --share', shell=True, stdout=subprocess.PIPE).stdout.read()
 reader = csv.reader([response], dialect='excel')
 for row in reader:
 	row[6] = decimal.Decimal(row[6])/1000000
 	row[7] = decimal.Decimal(row[7])/1000000
         payload = {"server_id": row[0],"sponsor": row[1],"server_name": row[2],"timestamp": row[3],"distance": row[4],"ping": row[5],"download": row[6],"upload": row[7],"share": row[8],"ip_address": row[9]}
-        r = requests.put("https://dev-api.mydigi.tech/log", data=payload)
+        r = requests.put(endpoint, data=payload)
 	print csv2string(row)
