d692b3a13ec089d4624397052c88eccc64009a28
Miguel Ibarra <miguel@digitec.ws>
2018-09-23 12:52:44 -0700

    create new script

---
+++ speedtest-rest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import csv
+import decimal
+import subprocess
+import StringIO
+import requests
+
+def csv2string(data):
+    si = StringIO.StringIO()
+    cw = csv.writer(si)
+    cw.writerow(data)
+    return si.getvalue().strip('\r\n')
+
+response = subprocess.Popen('/home/pi/speedtest/bin/speedtest-cli --csv  --share', shell=True, stdout=subprocess.PIPE).stdout.read()
+reader = csv.reader([response], dialect='excel')
+for row in reader:
+	row[6] = decimal.Decimal(row[6])/1000000
+	row[7] = decimal.Decimal(row[7])/1000000
+        payload = {"server_id": row[0],"sponsor": row[1],"server_name": row[2],"timestamp": row[3],"distance": row[4],"ping": row[5],"download": row[6],"upload": row[7],"share": row[8],"ip_address": row[9]}
+        r = requests.put("https://dev-api.mydigi.tech/log", data=payload)
+	print csv2string(row)
