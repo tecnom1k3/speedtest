d858ead43c077d573a1a516a6a4c771f7bda157c
Miguel Ibarra <miguel@digitec.ws>
2018-09-23 14:18:38 -0700

    trying to fix issue with rest endpoint

--- speedtest-rest.py
+++ speedtest-rest.py
@@ -16,9 +16,18 @@ def csv2string(data):
     cw.writerow(data)
     return si.getvalue().strip('\r\n')
 
-endpoint = os.getenv("LOG_ENPOINT")
+url = os.getenv("LOG_ENPOINT")
 apiKey = os.getenv("APY_KEY")
-headers = {'x-api-key': apiKey, 'Content-Type': 'application/json'}
+headers = {
+    'Content-Type': "application/json",
+    'x-api-key': apiKey,
+    }
+payload = "{\n  \"server_id\": 1782,\n  \"sponsor\": \"Comcast\",\n  \"server_name\": \"Seattle, WA\",\n  \"timestamp\": \"2018-09-21T05:10:01.930144Z\",\n  \"distance\": 15.042833314578802,\n  \"ping\": 25.612,\n  \"download\": 82.68025586921611,\n  \"upload\": 11.582651480139456,\n  \"share\": \"http://www.speedtest.net/result/7653589584.png\",\n  \"ip_address\": \"73.169.246.76\"\n}"
+
+#response = requests.request("PUT", url, data=payload, headers=headers)
+
+#print(response.text)
+
 speedtestCliBin = os.getenv("SPEED_TEST_CLI_BIN")
 
 response = subprocess.Popen(speedtestCliBin + ' --csv  --share', shell=True, stdout=subprocess.PIPE).stdout.read()
@@ -26,6 +35,8 @@ reader = csv.reader([response], dialect='excel')
 for row in reader:
 	row[6] = decimal.Decimal(row[6])/1000000
 	row[7] = decimal.Decimal(row[7])/1000000
-        payload = {"server_id": row[0],"sponsor": row[1],"server_name": row[2],"timestamp": row[3],"distance": row[4],"ping": row[5],"download": row[6],"upload": row[7],"share": row[8],"ip_address": row[9]}
-        r = requests.put(endpoint, data=payload, headers=headers)
+        #payload = {"server_id": row[0],"sponsor": row[1],"server_name": row[2],"timestamp": row[3],"distance": row[4],"ping": row[5],"download": row[6],"upload": row[7],"share": row[8],"ip_address": row[9]}
+        r = requests.request("PUT", url, data=payload, headers=headers)
 	print csv2string(row)
+        print r.status_code
+        print r.text
