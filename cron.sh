ba9ee58f96ba579de2dcb4e35431c8319a731e57
Miguel Ibarra <miguel@digitec.ws>
2018-09-23 23:31:38 +0000

    changing shell to use new script, formatting original script

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-/usr/bin/python /home/pi/speedtest/speedtest.py >> /home/pi/speedtest/data/data.csv
+/usr/bin/python /home/pi/speedtest/speedtest-rest.py >> /home/pi/speedtest/data/data.csv
 /usr/local/bin/aws s3 sync /home/pi/speedtest/data s3://comcast-speedtest
