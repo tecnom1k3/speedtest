be3d879993c24e8c040b22c068efac84592620b9
Miguel Ibarra <miguel@digitec.ws>
2018-09-23 22:18:50 +0000

    fixing rest call

--- speedtest-rest.py
+++ speedtest-rest.py
@@ -8,28 +8,33 @@ import requests
 import os
 
 from dotenv import load_dotenv
+
 load_dotenv(dotenv_path='.env')
 
+
 def csv2string(data):
-	si = StringIO.StringIO()
-	cw = csv.writer(si)
-	cw.writerow(data)
-	return si.getvalue().strip('\r\n')
+    si = StringIO.StringIO()
+    cw = csv.writer(si)
+    cw.writerow(data)
+    return si.getvalue().strip('\r\n')
+
 
 url = os.getenv("LOG_ENPOINT")
 apiKey = os.getenv("APY_KEY")
 headers = {'Content-Type': "application/json", 'x-api-key': apiKey}
 
-speedtestCliBin = os.getenv("SPEED_TEST_CLI_BIN")
+speedTestCliBin = os.getenv("SPEED_TEST_CLI_BIN")
 
-response = subprocess.Popen(speedtestCliBin + ' --csv  --share', shell=True, stdout=subprocess.PIPE).stdout.read()
+response = subprocess.Popen(speedTestCliBin + ' --csv  --share', shell=True, stdout=subprocess.PIPE).stdout.read()
 reader = csv.reader([response], dialect='excel')
 for row in reader:
-	row[6] = decimal.Decimal(row[6])/1000000
-	row[7] = decimal.Decimal(row[7])/1000000
-	#payload = {"server_id": row[0],"sponsor": row[1],"server_name": row[2],"timestamp": row[3],"distance": row[4],"ping": row[5],"download": row[6],"upload": row[7],"share": row[8],"ip_address": row[9]}
-	payload = "{  \"server_id\": " + row[0] + ",  \"sponsor\": \"" + row[1] + "\",  \"server_name\": \"" + row[2] + "\",  \"timestamp\": \"" + row[3] + "\",  \"distance\": " + row[4] + ",  \"ping\": " + row[5] + ",  \"download\": " + row[6] + ",  \"upload\": " + row[7] + ",  \"share\": \"" + row[8] + "\",  \"ip_address\": \"" + row[9] + "\"}"
-	r = requests.request("PUT", url, data=payload, headers=headers)
-	print csv2string(row)
-	print r.status_code
-	print r.text
+    row[6] = decimal.Decimal(row[6]) / 1000000
+    row[7] = decimal.Decimal(row[7]) / 1000000
+    payload = "{  \"server_id\": " + row[0] + ",  \"sponsor\": \"" + row[1] + "\",  \"server_name\": \"" + row[
+        2] + "\",  \"timestamp\": \"" + row[3] + "\",  \"distance\": " + row[4] + ",  \"ping\": " + row[
+                  5] + ",  \"download\": " + str(row[6]) + ",  \"upload\": " + str(row[7]) + ",  \"share\": \"" + row[
+                  8] + "\",  \"ip_address\": \"" + row[9] + "\"}"
+    r = requests.request("PUT", url, data=payload, headers=headers)
+    print csv2string(row)
+    # print r.status_code
+    # print r.text
